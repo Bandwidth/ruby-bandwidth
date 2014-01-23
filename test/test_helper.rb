1d959f900cebc576b5b9fa9c6f4b2bc00448b2d1
Phil Pirozhkov <pirjsuka@gmail.com>
2014-01-23 16:37:51 +0700

    check if user, token and secret are being passed to API

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require_relative '../lib/bandwidth.rb'
 module Bandwidth
   class StubbedConnection < Connection
     def http
-      @http ||= StubbedHttp.new
+      @http ||= StubbedHttp.new @user_id, @token, @secret
     end
 
     alias :short_http :http
@@ -14,13 +14,10 @@ module Bandwidth
     delegate :stub, to: :http
 
     class StubbedHttp < HTTP
-      def initialize
-        super "user_id", "token", "secret"
-      end
-
       def connection
         @connection ||= Faraday.new do |faraday|
           faraday.adapter :test, @stubs
+          faraday.basic_auth @token, @secret
         end
       end
 
