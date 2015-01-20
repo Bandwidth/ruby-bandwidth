3a6708d6442f7a5241595d38d773b73dc2adf268
Andrey Belchikov <belchikovs@gmail.com>
2015-01-20 01:00:47 -0800

    Added Call

--- lib/bandwidth/call.rb
+++ lib/bandwidth/call.rb
@@ -4,5 +4,110 @@ module Bandwidth
     extend ClientWrapper
     include ApiItem
 
+    def self.get(client, id)
+      item = client.make_request(:get, client.concat_user_path("#{CALL_PATH}/#{id}"))[0]
+      Call.new(item, client)
+    end
+    wrap_client_arg :get
+
+    def self.list(client, query = nil)
+      client.make_request(:get, client.concat_user_path(CALL_PATH), query)[0].map do |item|
+        Call.new(item, client)
+      end
+    end
+    wrap_client_arg :list
+
+    def self.create(client, data)
+      headers = client.make_request(:post, client.concat_user_path(CALL_PATH), data)[1]
+      id = Client.get_id_from_location_header(headers[:location])
+      self.get(client, id)
+    end
+    wrap_client_arg :create
+
+    def update(data)
+      @client.make_request(:post, @client.concat_user_path("#{CALL_PATH}/#{id}"), data)[0]
+    end
+
+    def play_audio(data)
+      @client.make_request(:post, @client.concat_user_path("#{CALL_PATH}/#{id}/audio"), data)[0]
+    end
+
+    def speak_sentence(sentence, tag = nil)
+      play_audio({:gender => "female", :locale => "en_US",
+                  :voice => "kate", :sentence => sentence, :tag => tag})
+    end
+
+    def play_recording(url)
+      play_audio({:file_url => url})
+    end
+
+    def set_dtmf(dtmf)
+      @client.make_request(:post, @client.concat_user_path("#{CALL_PATH}/#{id}/dtmf"), {:dtmf_out => dtmf})[0]
+    end
+
+    def create_gather(data)
+      d = if data.is_a?(String)
+            {
+              :tag => id, :max_digits => 1,
+              :prompt => {:locale => 'en_US', :gender => 'female', :sentence => data, :voice => 'kate', :bargeable => true }
+            }
+          else
+            data
+          end
+      headers = @client.make_request(:post, @client.concat_user_path("#{CALL_PATH}/#{id}/gather"), d)[1]
+      id = Client.get_id_from_location_header(headers[:location])
+      get_gather(id)
+    end
+
+    def update_gather(gather_id, data)
+      @client.make_request(:post, @client.concat_user_path("#{CALL_PATH}/#{id}/gather/#{gather_id}"), data)[0]
+    end
+
+    def get_gather(gather_id)
+      @client.make_request(:get, @client.concat_user_path("#{CALL_PATH}/#{id}/gather/#{gather_id}"))[0]
+    end
+
+    def get_event(event_id)
+      @client.make_request(:get, @client.concat_user_path("#{CALL_PATH}/#{id}/events/#{event_id}"))[0]
+    end
+
+    def get_events()
+      @client.make_request(:get, @client.concat_user_path("#{CALL_PATH}/#{id}/events"))[0]
+    end
+
+    def get_recordings()
+      @client.make_request(:get, @client.concat_user_path("#{CALL_PATH}/#{id}/recordings"))[0]
+    end
+
+    def hangup()
+      update({:state => 'completed'})
+      reload()
+    end
+
+    def answer_on_incoming()
+      update({:state => 'active'})
+      reload()
+    end
+
+    def reject_incoming()
+      update({:state => 'rejected'})
+      reload()
+    end
+
+    def recording_on()
+      update({:recording_enabled => true})
+      reload()
+    end
+
+    def recording_off()
+      update({:recording_enabled => false})
+      reload()
+    end
+
+    protected
+
+    def reload()
+      @data.merge!(@client.make_request(:get, @client.concat_user_path("#{CALL_PATH}/#{id}"))[0])
+    end
   end
 end
---
+++ spec/bandwidth/call_spec.rb
@@ -0,0 +1,181 @@
+describe Bandwidth::Call do
+  client = nil
+  template_json = '{"id":"1","state":"active"}'
+  template_item = {:id=>'1', :state=>'active'}
+  before :each do
+    client = Helper.get_client()
+  end
+
+  after :each do
+    client.stubs.verify_stubbed_calls()
+  end
+
+  describe '#get' do
+    it 'should return specific item' do
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, template_json]}
+      expect(Call.get(client, '1').to_data()).to eql(template_item)
+    end
+  end
+
+  describe '#list' do
+    it 'should return items' do
+      client.stubs.get('/v1/users/userId/calls') {|env| [200, {}, "[#{template_json}]"]}
+      expect(Call.list(client).map {|i| i.to_data()}).to eql([template_item])
+    end
+  end
+
+  describe '#create' do
+    it 'should create new item' do
+      client.stubs.post('/v1/users/userId/calls', '{"from":"from","to":"to"}') {|env| [201, {'Location' => '/v1/users/userId/calls/1'}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, template_json]}
+      expect(Call.create(client, {:from=>'from', :to=>'to'}).to_data()).to eql(template_item)
+    end
+  end
+
+  describe '#update' do
+    it 'should change item' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"state":"active"}') {|env| [200, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.update({:state=>'active'})
+    end
+  end
+
+  describe '#play_audio' do
+    it 'should play audio' do
+      client.stubs.post('/v1/users/userId/calls/1/audio', '{"fileUrl":"http://host1"}') {|env| [200, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.play_audio(:file_url=>'http://host1')
+    end
+  end
+
+  describe '#speak_sentence' do
+    it 'should speak a sentence' do
+      client.stubs.post('/v1/users/userId/calls/1/audio', '{"gender":"female","locale":"en_US","voice":"kate","sentence":"test","tag":null}') {|env| [200, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.speak_sentence('test')
+    end
+  end
+
+  describe '#play_recording' do
+    it 'should play recording' do
+      client.stubs.post('/v1/users/userId/calls/1/audio', '{"fileUrl":"http://host1"}') {|env| [200, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.play_recording('http://host1')
+    end
+  end
+
+  describe '#set_dtmf' do
+    it 'should set dtmf value' do
+      client.stubs.post('/v1/users/userId/calls/1/dtmf', '{"dtmfOut":"value"}') {|env| [200, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.set_dtmf('value')
+    end
+  end
+
+  describe '#create_gather' do
+    json =  '{"tag":"1","maxDigits":1,"prompt":{"locale":"en_US","gender":"female","sentence":"test","voice":"kate","bargeable":true}}'
+    it 'should create a gather' do
+      client.stubs.post('/v1/users/userId/calls/1/gather', json) {|env| [200, {'Location' => '/v1/users/userId/calls/1/gather/10'}, '']}
+      client.stubs.get('/v1/users/userId/calls/1/gather/10') {|env| [200, {}, '{"id": "10"}']}
+      item = Call.new({:id=>'1'}, client)
+      data = {:tag => "1", :maxDigits => 1, :prompt => {:locale => "en_US", :gender => "female", :sentence => "test", :voice => "kate", :bargeable => true}}
+      expect(item.create_gather(data)[:id]).to eql('10')
+    end
+    it 'should create a gather by sentence sting only' do
+      client.stubs.post('/v1/users/userId/calls/1/gather', json) {|env| [200, {'Location' => '/v1/users/userId/calls/1/gather/10'}, '']}
+      client.stubs.get('/v1/users/userId/calls/1/gather/10') {|env| [200, {}, '{"id": "10"}']}
+      item = Call.new({:id=>'1'}, client)
+      expect(item.create_gather('test')[:id]).to eql('10')
+    end
+  end
+
+  describe '#update_gather' do
+    it 'should update a gather' do
+      client.stubs.post('/v1/users/userId/calls/1/gather/10', '{"state":"completed"}') {|env| [201, {}, '']}
+      item = Call.new({:id=>'1'}, client)
+      item.update_gather('10', {:state => 'completed'})
+    end
+  end
+
+  describe '#get_gather' do
+    it 'should return a gather' do
+      client.stubs.get('/v1/users/userId/calls/1/gather/10') {|env| [200, {}, '{"id": "10"}']}
+      item = Call.new({:id=>'1'}, client)
+      expect(item.get_gather('10')[:id]).to eql('10')
+    end
+  end
+
+  describe '#get_event' do
+    it 'should return a event' do
+      client.stubs.get('/v1/users/userId/calls/1/events/10') {|env| [200, {}, '{"id": "10"}']}
+      item = Call.new({:id=>'1'}, client)
+      expect(item.get_event('10')[:id]).to eql('10')
+    end
+  end
+
+  describe '#get_events' do
+    it 'should return events of a call' do
+      client.stubs.get('/v1/users/userId/calls/1/events') {|env| [200, {}, '[{"id": "10"},{"id": "11"}]']}
+      item = Call.new({:id=>'1'}, client)
+      expect(item.get_events().map {|i| i[:id]}).to eql(['10', '11'])
+    end
+  end
+
+  describe '#get_recording' do
+    it 'should return recordings of a call' do
+      client.stubs.get('/v1/users/userId/calls/1/recordings') {|env| [200, {}, '[{"id": "10"},{"id": "11"}]']}
+      item = Call.new({:id=>'1'}, client)
+      expect(item.get_recordings().map {|i| i[:id]}).to eql(['10', '11'])
+    end
+  end
+
+  describe '#hangup' do
+    it 'should hang up a call' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"state":"completed"}') {|env| [200, {}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, '{"state": "completed"}']}
+      item = Call.new({:id=>'1', :state=>'active'}, client)
+      item.hangup()
+      expect(item.state).to eql('completed')
+    end
+  end
+
+  describe '#answer_on_incoming' do
+    it 'should answer on an incoming call' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"state":"active"}') {|env| [200, {}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, '{"state": "active"}']}
+      item = Call.new({:id=>'1', :state=>'active'}, client)
+      item.answer_on_incoming()
+      expect(item.state).to eql('active')
+    end
+  end
+
+  describe '#reject_incoming' do
+    it 'should reject  an incoming call' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"state":"rejected"}') {|env| [200, {}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, '{"state": "rejected"}']}
+      item = Call.new({:id=>'1', :state=>'active'}, client)
+      item.reject_incoming()
+      expect(item.state).to eql('rejected')
+    end
+  end
+
+  describe '#recording_on' do
+    it 'should tune on recording of a call' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"recordingEnabled":true}') {|env| [200, {}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, '{"recordingEnabled":true}']}
+      item = Call.new({:id=>'1', :recording_enabled=>false}, client)
+      item.recording_on()
+      expect(item.recording_enabled).to eql(true)
+    end
+  end
+
+  describe '#recording_off' do
+    it 'should tune off recording of a call' do
+      client.stubs.post('/v1/users/userId/calls/1', '{"recordingEnabled":false}') {|env| [200, {}, '']}
+      client.stubs.get('/v1/users/userId/calls/1') {|env| [200, {}, '{"recordingEnabled":false}']}
+      item = Call.new({:id=>'1', :recording_enabled=>true}, client)
+      item.recording_off()
+      expect(item.recording_enabled).to eql(false)
+    end
+  end
+end
