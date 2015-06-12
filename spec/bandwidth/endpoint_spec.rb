70d0c122a6528f8925d02a0797e0d310de4c93f5
Andrey Belchikov <belchikovs@gmail.com>
2015-06-11 22:41:55 -0700

    Added token actions

--- spec/bandwidth/endpoint_spec.rb
+++ spec/bandwidth/endpoint_spec.rb
@@ -16,4 +16,24 @@ describe Bandwidth::EndPoint do
       item.delete()
     end
   end
+
+  describe '#create_auth_token' do
+    it 'should create auth token' do
+      client.stubs.post('/v1/users/userId/domains/1/endpoints/10/tokens') {|env| [201, {}, '{"expires":100, "token":"123"}']}
+      item = EndPoint.new({:id=>'10'}, client)
+      item.domain_id = '1'
+      t = item.create_auth_token()
+      expect(t[:expires]).to eql(100)
+      expect(t[:token]).to eql('123')
+    end
+  end
+
+  describe '#delete_auth_token' do
+    it 'should delete auth token' do
+      client.stubs.delete('/v1/users/userId/domains/1/endpoints/10/tokens/123') {|env| [200, {}, '']}
+      item = EndPoint.new({:id=>'10'}, client)
+      item.domain_id = '1'
+      item.delete_auth_token('123')
+    end
+  end
 end
