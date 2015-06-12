416a9303b8582110ad0d530e69eb17fcf479d849
Andrey Belchikov <belchikovs@gmail.com>
2015-06-11 22:41:55 -0700

    Added token actions

--- lib/bandwidth/endpoint.rb
+++ lib/bandwidth/endpoint.rb
@@ -12,5 +12,20 @@ module Bandwidth
     end
 
     alias_method :destroy, :delete
+
+
+    # Create auth token.
+    # @example
+    #   token = endpoint.create_auth_token()
+    def create_auth_token()
+      @client.make_request(:post, @client.concat_user_path("#{DOMAIN_PATH}/#{@domain_id}/endpoints/#{id}/tokens"))[0]
+    end
+
+    # Delete auth token.
+    # @example
+    #   endpoint.delete_auth_token('token')
+    def delete_auth_token(token)
+      @client.make_request(:delete, @client.concat_user_path("#{DOMAIN_PATH}/#{@domain_id}/endpoints/#{id}/tokens/#{token}"))[0]
+    end
   end
 end
--- lib/bandwidth/version.rb
+++ lib/bandwidth/version.rb
@@ -1,4 +1,4 @@
 module Bandwidth
   # Version of this gem
-  VERSION = "1.0.8"
+  VERSION = "1.0.9"
 end
