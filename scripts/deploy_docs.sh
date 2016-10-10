b718896e80632ba809665adab26639ca457f82bd
Unknown <belchikovs@gmail.com>
2016-10-10 16:18:56 +0300

    Some fixes

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Update docs only on 'master' changes (and only for ruby 2.3)
-[ "$TRAVIS_RUBY_VERSION" == "2.3.0" ] || exit 0
+# Update docs only on 'master' changes (only for ruby 2.3 build)
+[[ $TRAVIS_RUBY_VERSION =~ ^2\.3\..* ]] || exit 0
 [ "$TRAVIS_BRANCH" == "master" ] || exit 0
 [ "$TRAVIS_EVENT_TYPE" == "push" ] || exit 0 # after apply PR
-rake yard:build
-git add -f ./doc
+
+
+# Publish to github
 git config user.name "Travis CI"
 git config user.email "$TRAVIS_EMAIL"
-git commit -m 'Update docs'
 echo machine github.com > ~/.netrc
 echo login $TRAVIS_GITHUB_ACCOUNT >> ~/.netrc
 echo password $TRAVIS_GITHUB_TOKEN >> ~/.netrc
 
-# Publish to github
 rake yard:publish
