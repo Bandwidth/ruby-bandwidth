6992535afd86af5f25fa0db5fdeae7552c0cb797
Travis CI <belchikovs@gmail.com>
2016-10-12 15:38:45 +0300

    Added missing methods for AvailableNumbers

--- lib/bandwidth/available_number.rb
+++ lib/bandwidth/available_number.rb
@@ -26,5 +26,33 @@ module Bandwidth
       client.make_request(:get, "#{AVAILABLE_NUMBER_PATH}/local", query)[0]
     end
     wrap_client_arg :search_local
+
+    # Search and order for available toll free numbers
+    # @param client [Client] optional client instance to make requests
+    # @param query [Hash} hash with parameters to search: quantity
+    # @return [Array] search result
+    # @example
+    #   result = AvailableNumber.search_and_order_toll_free(client, :quantity=>20)
+    def self.search_and_order_toll_free(client, query = nil)
+      client.make_request(:post, "#{AVAILABLE_NUMBER_PATH}/tollFree", {},  query)[0].map do |item|
+        item[:id] = Client.get_id_from_location_header(item[:location])
+        item
+      end
+    end
+    wrap_client_arg :search_and_order_toll_free
+
+    # Search for available local numbers and order them
+    # @param client [Client] optional client instance to make requests
+    # @param query [Hash] hash with parametes to search: city, state, zip, areaCode, localNumber, inLocalCallingArea, quantity
+    # @return [Array] search result
+    # @example
+    #   result = AvailableNumber.search_and_order_local(client, {:state=>"state", :zip=>"zip", :area_code=>"code"})
+    def self.search_and_order_local(client, query = nil)
+      client.make_request(:post, "#{AVAILABLE_NUMBER_PATH}/local", {}, query)[0].map do |item|
+        item[:id] = Client.get_id_from_location_header(item[:location])
+        item
+      end
+    end
+    wrap_client_arg :search_and_order_local
   end
 end
