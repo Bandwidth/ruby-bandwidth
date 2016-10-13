1db0f60dde9d9bb38088fb294825726708c07963
Travis CI <belchikovs@gmail.com>
2016-10-13 10:15:33 +0300

    Added missing test for LazyEnumerator

--- spec/bandwidth/lazy_enumerator_spec.rb
+++ spec/bandwidth/lazy_enumerator_spec.rb
@@ -18,6 +18,17 @@ describe Bandwidth::LazyEnumerator do
     expect(list).to match_array([11, 12, 13])
   end
 
+  it 'should require data on demand' do
+    called = false
+    list = Bandwidth::LazyEnumerator.new(lambda do
+      called = true
+      [[1, 2, 3], {}]
+    end, client)
+    expect(called).to be false
+    list.to_a
+    expect(called).to be true
+  end
+
   it 'should handle header "Link"' do
     list = Bandwidth::LazyEnumerator.new(lambda {[[1, 2, 3], {
         link: '<https://api.catapult.inetwork.com/v1/users/userId/account/transactions?page=0&size=25>; rel="first"'
