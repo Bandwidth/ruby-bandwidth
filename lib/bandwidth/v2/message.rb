2f0f4d8ffe02041c05163b7b169c0ba0f28cd803
jmulford-bandwidth <jmulford@bandwidth.com>
2019-04-03 15:51:05 -0400

    Fixed url generation for v2m requests

--- lib/bandwidth/v2/message.rb
+++ lib/bandwidth/v2/message.rb
@@ -14,7 +14,7 @@ module Bandwidth
       # @example
       #   message = Message.create(client, {:from=>"from", :to=>["to"], :text=>"text", :application_id=>"messagingApplicationId"})
       def self.create(client, data)
-        client.make_request(:post, client.concat_user_path(MESSAGE_PATH), data, 'v2', 'https://messaging.bandwidth.com/api')[0]
+        client.make_request(:post, client.concat_user_path(MESSAGE_PATH), data, 'api/v2', 'https://messaging.bandwidth.com')[0]
       end
       wrap_client_arg :create
 
