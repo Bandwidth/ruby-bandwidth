ddcc71e3bad5cca0e3bcb79a29b2ddb751e37747
jmulford-bandwidth <jmulford@bandwidth.com>
2021-01-15 10:51:28 -0500

    updated test

--- spec/bandwidth/client_spec.rb
+++ spec/bandwidth/client_spec.rb
@@ -73,7 +73,7 @@ describe Bandwidth::Client do
     end
 
     it 'should raise error if http status >= 400' do
-      client.stubs.get('/v1/path1') { |env| [400, {}, '{"code": "code", "message": "error"}'] }
+      client.stubs.get('/v1/path1') { |env| [400, {}, 'error'] }
       expect{client.make_request(:get, '/path1')}.to raise_error(Errors::GenericError, "error")
     end
   end
